e0f10a4be1afdff46ddf8e09ad95bc75fa94f528
Nehal-github <164448285+Nehal-github@users.noreply.github.com>
2025-08-11 01:49:28 +0500

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <header className="w-full text-center pt-5 pb-3">
       <h1 className="text-4xl md:text-5xl font-romantic text-pink-700 drop-shadow-lg">
-        "HAPPY BIRTHDAY REEEEMAL!" 💖
+        HAPPY BIRTHDAY REEEEMAL! 💖
       </h1>
     </header>
   );
